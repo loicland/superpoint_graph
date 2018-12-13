3a0ac4b69797647debb944e2e3cdc19164a0565c
Thomas Luo <tholuo@gmail.com>
2018-12-13 11:44:06 +0000

    1st commit, create a notebook for creating a single step point cloud segmentation process

---
+++ pcl_segmentation.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# # Point Cloud Segmentation
+
+# In[ ]:
+
+
+This notebook aims at coding a single step to process point cloud, i.e. regroup all from SuperpointInference.ipynb in a single class. The method will take the point cloud in input and will o
+
