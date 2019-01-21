a6d1b7f53ced62483d215ae1b39273a04de4bd11
Thomas Luo <tholuo@gmail.com>
2019-01-21 14:25:53 +0000

    removing unnecessary functions

--- providers/datasets.py
+++ providers/datasets.py
@@ -53,29 +53,6 @@ class HelixDataset:
             'inv_class_map': {value:key for (key,value) in self.labels.items()},
         }
     
-    def get_datasets(self, args, test_seed_offset=0):
-        """ Gets training and test datasets. """
-        # Load superpoints graphs
-        testlist, trainlist = [], []
-        for n in range(1,7):
-            if n != args.cvfold:
-                path = '{}/superpoint_graphs/Area_{:d}/'.format(args.S3DIS_PATH, n)
-                for fname in sorted(os.listdir(path)):
-                    if fname.endswith(".h5"):
-                        trainlist.append(spg.spg_reader(args, path + fname, True))
-        path = '{}/superpoint_graphs/Area_{:d}/'.format(args.S3DIS_PATH, args.cvfold)
-        for fname in sorted(os.listdir(path)):
-            if fname.endswith(".h5"):
-                testlist.append(spg.spg_reader(args, path + fname, True))
-        # Normalize edge features
-        if args.spg_attribs01:
-            trainlist, testlist = spg.scaler01(trainlist, testlist)
-
-        return tnt.dataset.ListDataset([spg.spg_to_igraph(*tlist) for tlist in trainlist],
-                                        functools.partial(spg.loader, train=True, args=args, db_path=args.S3DIS_PATH)), \
-               tnt.dataset.ListDataset([spg.spg_to_igraph(*tlist) for tlist in testlist],
-                                        functools.partial(spg.loader, train=False, args=args, db_path=args.S3DIS_PATH, test_seed_offset=test_seed_offset))
-    
     def get_data(self, args, filename, folder_s, test_seed_offset=0):
         """ get data for inference """
         # Load superpoints graphs
